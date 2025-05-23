66a09addfacfb26175a1acdf9e36b3b53b70abf2
AllenVillalobos <allenvs06@gmail.com>
2025-05-23 13:23:36 -0600

    Ejercisio 5 Numeros Primos

---
+++ Paginas/Ejercisio5.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Ejercisio5.aspx.cs" Inherits="TareaSemana2.Paginas.Ejercisio5" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label>Ingrese Un Numero</asp:Label>
+            <br />
+            <br />
+            <asp:TextBox ID="txtNumero" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="btnEnviar" runat="server" Text="Ver Numeros Primos" OnClick="primos" />
+            <br />
+            <br />
+            <asp:Label ID="lblPrimos" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Paginas/Ejercisio5.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TareaSemana2.Paginas
+{
+    public partial class Ejercisio5 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        public void primos(object sender, EventArgs e)
+        {
+            int limite = int.Parse(txtNumero.Text);
+            string listaDeNumeros = "";
+
+            for(int i=2; i <= limite; i++)
+            {
+                Boolean esPrimo = true;
+                for (int j=2; j <= Math.Sqrt(i); j++)
+                {
+                    if (i % j == 0)
+                    {
+                        esPrimo = false;
+                        break;
+                    }
+                }
+                if (esPrimo)
+                {
+                    listaDeNumeros += " " + i;
+                }
+            }
+
+            lblPrimos.Text = listaDeNumeros;
+            lblPrimos.ForeColor = System.Drawing.Color.Green;
+        }
+    }
+}
